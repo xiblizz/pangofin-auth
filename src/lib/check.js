156878a6d94b595105f704afce825e8336f6bad7
RM <m8robert@gmail.com>
2026-06-14 18:23:59 +0200

    type

--- src/lib/check.js
+++ src/lib/check.js
@@ -1,8 +1,8 @@
 export default async function checkAccess() {
     try {
-        const res = await fetch('/api/check')
+        let res = await fetch('/api/check')
         if (res.ok) {
-            const data = await res.json()
+            let data = await res.json()
             if (data.success) {
                 return {
                     hasAccess: data.success,
