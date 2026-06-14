a562c6bd30ba33b3617337df9bbb0c5696f36eef
RM <m8robert@gmail.com>
2026-06-14 18:28:46 +0200

    BASE_URL

--- src/lib/check.js
+++ src/lib/check.js
@@ -1,6 +1,8 @@
+import env from '$env/dynamic/public'
+
 export default async function checkAccess() {
     try {
-        let res = await fetch('/api/check')
+        let res = await fetch(`${env.BASE_URL}/api/check`)
         if (res.ok) {
             let data = await res.json()
             if (data.success) {
